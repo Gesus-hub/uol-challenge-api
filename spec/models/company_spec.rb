08250660da09d3208ffd274abe198c4c82ded9df
Vinicius Guima <vinicius.guimaraes@nowmed.com.br>
2025-01-30 15:56:38 -0300

    refactor: Add has_many users association in Company model and Update Company model and User queries tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Company do
+  describe 'associations' do
+    it { is_expected.to have_many(:users).dependent(:destroy) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
   end
